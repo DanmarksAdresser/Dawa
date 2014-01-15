534cda1c1b3b8992c979e5bd43b6d52d07de1f44
Peter Mechlenborg <pmm@trifork.com>
2014-01-15 14:45:46 +0100

    Forgot to change package.json and Gruntfile.js to point to server.js instead of dawa.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
     express: {
       test: {
         options: {
-          script: 'dawa.js',
+          script: 'server.js',
           output: 'Express server listening'
         }
       }
