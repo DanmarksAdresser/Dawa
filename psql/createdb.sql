732e9e2f61e8cad684e98b2fa283e5dc22b99e1e
Anders Hessellund Jensen <ahj@trifork.com>
2014-02-11 15:13:28 +0100

    Tilfoej parameter til createdb.sql

--- psql/createdb.sql
+++ psql/createdb.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE dawa ENCODING 'UTF-8' LC_COLLATE 'da_DK' LC_CTYPE 'da_DK' TEMPLATE template0;
-\c dawa;
+CREATE DATABASE dawatest ENCODING 'UTF-8' LC_COLLATE 'da_DK' LC_CTYPE 'da_DK' TEMPLATE template0;
+ALTER DATABASE dawatest SET cursor_tuple_fraction = 0.001;
+\c dawatest;
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
