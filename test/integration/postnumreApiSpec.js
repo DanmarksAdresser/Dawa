cd638545faf345dd49fe740f287860c464a72e61
Peter Mechlenborg <pmm@trifork.com>
2014-02-18 14:20:33 +0100

    Updating tests for new base url

--- test/integration/postnumreApiSpec.js
+++ test/integration/postnumreApiSpec.js
@@ -5,15 +5,15 @@ var request = require("request");
 
 describe("PostnumreApi", function() {
   it("It is possible to get a single postnummer", function(done) {
-    request.get({url: "http://localhost:3000/api/pg/postnumre/8600", json: true}, function(error, response, result) {
+    request.get({url: "http://localhost:3000/postnumre/8600", json: true}, function(error, response, result) {
       expect(result.nr).toBe(8600);
       expect(result.navn).toBe("Silkeborg");
-      expect(result.kommuner).toEqual([{ href : 'http://dawa.aws.dk/api/pg/kommuner/740', kode : 740, navn : 'Silkeborg' }]);
+      expect(result.kommuner).toEqual([{ href : 'http://dawa.aws.dk/kommuner/740', kode : 740, navn : 'Silkeborg' }]);
       done();
     });
   });
   it("It is possible to autocomplete a postnummer", function(done) {
-    request.get({url: "http://localhost:3000/api/pg/postnumre/autocomplete?q=silkeb", json: true}, function(error, response, result) {
+    request.get({url: "http://localhost:3000/postnumre/autocomplete?q=silkeb", json: true}, function(error, response, result) {
       var suggestion = result[0];
       expect(suggestion.tekst).toBe('8600 Silkeborg');
       expect(suggestion.postnummer.nr).toBe(8600);
@@ -22,31 +22,31 @@ describe("PostnumreApi", function() {
   });
 
   it('It is possible to search for postnumre', function(done) {
-    request.get({url: "http://localhost:3000/api/pg/postnumre?q=silkeborg", json: true}, function(error, response, result) {
+    request.get({url: "http://localhost:3000/postnumre?q=silkeborg", json: true}, function(error, response, result) {
       var postnummer = result[0];
       expect(postnummer.nr).toBe(8600);
       expect(postnummer.navn).toBe('Silkeborg');
-      expect(postnummer.kommuner).toEqual([{ href : 'http://dawa.aws.dk/api/pg/kommuner/740', kode : 740, navn : 'Silkeborg' }]);
+      expect(postnummer.kommuner).toEqual([{ href : 'http://dawa.aws.dk/kommuner/740', kode : 740, navn : 'Silkeborg' }]);
       done();
     });
   });
 
   it('It is possible to search for postnumre by name', function(done) {
-    request.get({url: "http://localhost:3000/api/pg/postnumre?navn=Silkeborg", json: true}, function(error, response, result) {
+    request.get({url: "http://localhost:3000/postnumre?navn=Silkeborg", json: true}, function(error, response, result) {
       var postnummer = result[0];
       expect(postnummer.nr).toBe(8600);
       expect(postnummer.navn).toBe('Silkeborg');
-      expect(postnummer.kommuner).toEqual([{ href : 'http://dawa.aws.dk/api/pg/kommuner/740', kode : 740, navn : 'Silkeborg' }]);
+      expect(postnummer.kommuner).toEqual([{ href : 'http://dawa.aws.dk/kommuner/740', kode : 740, navn : 'Silkeborg' }]);
       done();
     });
   });
 
   it('It is possible to search for postnumre by kommunekode', function(done) {
-    request.get({url: "http://localhost:3000/api/pg/postnumre?kommune=740", json: true}, function(error, response, result) {
+    request.get({url: "http://localhost:3000/postnumre?kommune=740", json: true}, function(error, response, result) {
       var postnummer = result[0];
       expect(postnummer.nr).toBe(8600);
       expect(postnummer.navn).toBe('Silkeborg');
-      expect(postnummer.kommuner).toEqual([{ href : 'http://dawa.aws.dk/api/pg/kommuner/740', kode : 740, navn : 'Silkeborg' }]);
+      expect(postnummer.kommuner).toEqual([{ href : 'http://dawa.aws.dk/kommuner/740', kode : 740, navn : 'Silkeborg' }]);
       done();
     });
   });
