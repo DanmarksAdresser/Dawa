622bd95a1581f06b8adceebbe2e1fcbc938634d9
Peter Mechlenborg <pmm@trifork.com>
2014-02-24 07:09:49 +0100

    Adding loggly logging

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 "use strict";
 
-var express             = require("express");
-var dawaPgApi           = require('./dawaPgApi');
-var apiSpec             = require('./apiSpec');
-var apiSpecUtil         = require('./apiSpecUtil');
-var parameterDoc        = require('./parameterDoc');
-var docUtil             = require('./docUtil');
+var express        = require("express");
+var winston        = require('winston');
+var expressWinston = require('express-winston');
+var dawaPgApi      = require('./dawaPgApi');
+var apiSpec        = require('./apiSpec');
+var apiSpecUtil    = require('./apiSpecUtil');
+var parameterDoc   = require('./parameterDoc');
+var docUtil        = require('./docUtil');
+
 
 var app = express();
 
-var cluseringDisabled = process.env.clusteringDisabled === 'true';
-
-app.use(express.logger('[:date - :response-time] :remote-addr -- ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
+setupLogging(app);
 app.use(express.compress());
 app.use(express.static(__dirname + '/public', {maxAge: 86400000}));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-
-function jadeDocumentationParams(req) {
-  var protocol = req.connection.encrypted ? 'https' : 'http';
-  return {url: protocol + '://' + req.headers.host, apiSpec: apiSpec, parameterDoc: parameterDoc, apiSpecUtil: apiSpecUtil, docUtil: docUtil};
-}
 
 app.get('/', function (req, res) {
   res.render('home.jade', {url: req.headers.host});
@@ -60,7 +56,7 @@ app.get('/om', function (req, res) {
 });
 //(\/[^\.])
 app.get(/html$/i, function (req, res) {
-  console.log('html url: '+req.originalUrl.replace('.html','.json') + ', ' + decodeURIComponent(req.originalUrl.replace('.html','.json')));
+//  console.log('html url: '+req.originalUrl.replace('.html','.json') + ', ' + decodeURIComponent(req.originalUrl.replace('.html','.json')));
   res.render('kort.jade', {url: decodeURIComponent(req.originalUrl.replace('.html','.json'))});
 });
 
@@ -68,11 +64,7 @@ var cluster = require('cluster');
 var workers = {};
 var count = require('os').cpus().length;
 
-function spawn(){
-  var worker = cluster.fork();
-  workers[worker.pid] = worker;
-  return worker;
-}
+var cluseringDisabled = process.env.clusteringDisabled === 'true';
 
 if (cluster.isMaster && !cluseringDisabled) {
   for (var i = 0; i < count; i++) {
@@ -80,7 +72,7 @@ if (cluster.isMaster && !cluseringDisabled) {
   }
 
   cluster.on('exit', function(worker, code, signal) {
-    console.log('worker %d died (%s %s). restarting...', worker.process.pid, signal, code);
+    winston.info('worker %d died (%s %s). restarting...', worker.process.pid, signal, code);
     delete workers[worker.pid];
     spawn();
   });
@@ -92,5 +84,38 @@ if (cluster.isMaster && !cluseringDisabled) {
   app.use('', dawaPgApi.setupRoutes());
 
   app.listen(listenPort);
-  console.log("Express server listening on port %d in %s mode", listenPort, app.settings.env);
+  winston.info("Express server listening on port %d in %s mode", listenPort, app.settings.env);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//// Helper functions //////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+function spawn(){
+  var worker = cluster.fork();
+  workers[worker.pid] = worker;
+  return worker;
+}
+
+function setupLogging(app){
+  require('winston-loggly')
+  var expressTransports = [];
+  if (process.env.DAWALOGGLY){
+    var logglyOptions = {subdomain:  'dawa',
+                         inputToken: process.env.DAWALOGGLY,
+                         json:       true};
+
+    winston.add(winston.transports.Loggly, logglyOptions);
+    expressTransports.push(new winston.transports.Loggly(logglyOptions));
+    winston.info("Production mode. Setting up Loggly logging %s", process.env.DAWALOGGLY);
+  }
+
+  expressTransports.push(new winston.transports.Console());
+  app.use(expressWinston.logger({transports: expressTransports}));
+}
+
+function jadeDocumentationParams(req) {
+  var protocol = req.connection.encrypted ? 'https' : 'http';
+  return {url: protocol + '://' + req.headers.host, apiSpec: apiSpec, parameterDoc: parameterDoc, apiSpecUtil: apiSpecUtil, docUtil: docUtil};
 }
