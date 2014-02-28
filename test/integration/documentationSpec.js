220469f9e4d80c6c99606116eebc89f8280beeca
Peter Mechlenborg <pmm@trifork.com>
2014-02-28 09:10:33 +0100

    Updating documentationSpec checking for version 2 of the parameter doc

--- test/integration/documentationSpec.js
+++ test/integration/documentationSpec.js
@@ -3,18 +3,18 @@
 var _            = require('underscore');
 var parameterDoc = require('../../parameterDoc');
 var apiSpec      = require('../../apiSpec');
-var winston      = require('winston');
+//var winston      = require('winston');
 //var request    = require('request');
 
 var specsToTest = _.keys(parameterDoc);
 
-describe('Parameter documentation', function() {
+describe('Parameter documentation.', function() {
   specsToTest.forEach(function(specName) {
-    describe('Documentation for ' + specName, function() {
+    describe('Documentation for ' + specName+'.', function() {
       var spec = apiSpec[specName];
       var docSpec = parameterDoc[specName];
+      var parameterGroups = spec.parameterGroups;
       if (docSpec.docVersion === 1){
-        var parameterGroups = spec.parameterGroups;
         var parametersWhichShouldBeDocumented = _.reduce(parameterGroups, function(memo, parameterGroup) {
           return memo.concat(parameterGroup.parameters);
         }, []);
@@ -31,12 +31,42 @@ describe('Parameter documentation', function() {
           });
         });
       } else {
-        winston.info('=============== TODO: document version 2 =================================');
-        winston.info('=============== TODO: document version 2 =================================');
-        winston.info('=============== TODO: document version 2 =================================');
-        winston.info('=============== TODO: document version 2 =================================');
-        winston.info('=============== TODO: document version 2 =================================');
+
+        // Checking version 2 of parameter doc.
+
+        var pg = parameterGroups;
+
+        checkPG(pg.reverseGeocoding, 'reverse', reverseDocParems(docSpec, spec));
+        checkPG(pg.crs,              'reverse', reverseDocParems(docSpec, spec));
+
+        checkPG(pg.crs,            'search',  searchDocParems(docSpec, spec));
+        checkPG(pg.propertyfilter, 'search',  searchDocParems(docSpec, spec));
+        checkPG(pg.geomWithin,     'search',  searchDocParems(docSpec, spec));
+        checkPG(pg.search,         'search',  searchDocParems(docSpec, spec));
+
+        checkPG(pg.crs,            'autocomplete',  autocompleteDocParems(docSpec, spec));
+        checkPG(pg.propertyfilter, 'autocomplete',  autocompleteDocParems(docSpec, spec));
+        checkPG(pg.geomWithin,     'autocomplete',  autocompleteDocParems(docSpec, spec));
+        checkPG(pg.search,         'autocomplete',  autocompleteDocParems(docSpec, spec));
+        checkPG(pg.autocomplete,   'autocomplete',  autocompleteDocParems(docSpec, spec));
       }
     });
   });
 });
+
+function reverseDocParems     (docSpec, spec){ return docSpec.resources['/'+spec.model.plural+'/reverse'].parameters; }
+function autocompleteDocParems(docSpec, spec){ return docSpec.resources['/'+spec.model.plural+'/autocomplete'].parameters; }
+function searchDocParems      (docSpec, spec){ return docSpec.resources['/'+spec.model.plural].parameters; }
+
+function checkPG(group, text, paramsInDoc){
+  if (group){
+    var paramsInDocMap = _.indexBy(paramsInDoc, 'name');
+    group.parameters.forEach(
+      function(param){
+        it('The parameter '+param.name+' should be documented in the '+text+' resource', function(){
+          expect(paramsInDocMap[param.name]).toBeDefined();
+        });
+      }
+    );
+  }
+}
