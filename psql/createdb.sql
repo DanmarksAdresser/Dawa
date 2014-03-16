8182eb1c445ebb4eec8b61e4e95a2aa1b6bcecd4
Anders Hessellund Jensen <ahj@trifork.com>
2014-03-16 23:08:03 +0100

    Saet nogle vigtige query planning parametre naar databasen oprettes

--- psql/createdb.sql
+++ psql/createdb.sql
@@ -1,5 +1,8 @@
 CREATE DATABASE dawa ENCODING 'UTF-8' LC_COLLATE 'da_DK.UTF-8' LC_CTYPE 'da_DK.UTF-8' TEMPLATE template0;
 ALTER DATABASE dawa SET cursor_tuple_fraction = 0.001;
+ALTER DATABASE dawa SET random_page_cost = 1.1;
+ALTER DATABASE dawa SET effective_cache_size='7GB';
+
 \c dawa;
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
