dc75d2edcb84d6af70db5514784be68d48547331
Peter Mechlenborg <pmm@trifork.com>
2014-03-17 14:58:54 +0100

    Delete the drop-columns.sql script

--- psql/drop-columns.sql
+++
@@ -1,8 +0,0 @@
-
-\set ON_ERROR_STOP on
-\set ECHO queries
-
-ALTER TABLE adgangsadresser DROP COLUMN ejerlavnavn;
-ALTER TABLE adgangsadresser DROP COLUMN postnrnavn;
-ALTER TABLE adgangsadresser DROP COLUMN vejnavn;
-
