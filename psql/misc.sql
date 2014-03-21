8b8effd0a42d04c7497f163749d1a3b1088cc1bb
Peter Mechlenborg <pmm@trifork.com>
2014-03-21 12:53:45 +0100

    Add first and last aggregate functions
    These can be used for JSON data, since JSON do not have any equality
    operator, and thus cannot be used in group by's or via the MAX(json)
    trick.

--- psql/misc.sql
+++ psql/misc.sql
@@ -31,3 +31,31 @@ DROP   TEXT SEARCH CONFIGURATION IF EXISTS adresser;
 CREATE TEXT SEARCH CONFIGURATION adresser (copy=simple);
 ALTER  TEXT SEARCH CONFIGURATION adresser ALTER MAPPING FOR asciiword,word,numword,asciihword,hword,numhword WITH simple;
 
+--
+-- From: https://wiki.postgresql.org/wiki/First/last_(aggregate)
+--
+-- Create a function that always returns the first non-NULL item
+CREATE OR REPLACE FUNCTION public.first_agg ( anyelement, anyelement )
+RETURNS anyelement LANGUAGE sql IMMUTABLE STRICT AS $$
+        SELECT $1;
+$$;
+
+-- And then wrap an aggregate around it
+CREATE AGGREGATE public.first (
+        sfunc    = public.first_agg,
+        basetype = anyelement,
+        stype    = anyelement
+);
+
+-- Create a function that always returns the last non-NULL item
+CREATE OR REPLACE FUNCTION public.last_agg ( anyelement, anyelement )
+RETURNS anyelement LANGUAGE sql IMMUTABLE STRICT AS $$
+        SELECT $2;
+$$;
+
+-- And then wrap an aggregate around it
+CREATE AGGREGATE public.last (
+        sfunc    = public.last_agg,
+        basetype = anyelement,
+        stype    = anyelement
+);
