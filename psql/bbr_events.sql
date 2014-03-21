674c56f07190a317bd67020818882aba7f92fa49
Anders Hessellund Jensen <ahj@trifork.com>
2014-03-21 13:18:13 +0100

    Add bbr_events table

---
+++ psql/bbr_events.sql
@@ -0,0 +1,21 @@
+\set ON_ERROR_STOP on
+\set ECHO queries
+
+DROP TABLE IF EXISTS bbr_events CASCADE;
+
+CREATE TABLE bbr_events(
+  sekvensnummer integer NOT NULL,
+  type varchar(255) NOT NULL,
+  bbrTidspunkt timestamp NOT NULL,
+  created timestamp NOT NULL,
+  data json NOT NULL,
+  PRIMARY KEY(sekvensnummer)
+);
+
+DROP FUNCTION IF EXISTS bbr_events_init() CASCADE;
+CREATE FUNCTION bbr_events_init() RETURNS void
+LANGUAGE plpgsql AS
+  $$
+  BEGIN
+  END;
+$$;
