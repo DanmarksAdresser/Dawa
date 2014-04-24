2a86c366453465396f7958cc174d0eaf851be877
Anders Hessellund Jensen <ahj@trifork.com>
2014-04-24 17:03:34 +0200

    Script til indlaesning af ordboeger

---
+++ psql/load-dicationary.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var async = require('async');
+
+var sqlCommon = require('./common');
+
+var cliParameterParsing = require('../bbr/common/cliParameterParsing');
+
+var optionSpec = {
+  pgConnectionUrl: [false, 'URL som anvendes ved forbindelse til databasen', 'string'],
+  version: [false, 'Version af ordbøger, som skal anvendes', 'string']
+};
+
+function exitOnErr(err){
+  if (err){
+    throw new Error(err);
+  }
+}
+cliParameterParsing.main(optionSpec,['pgConnectionUrl', 'version'], function(args, options) {
+  sqlCommon.withWriteTranaction(options.pgConnectionUrl, function(err, client, commit) {
+    exitOnErr(err);
+    async.series([
+      function(callback){
+        client.query('CREATE EXTENSION IF NOT EXISTS dict_xsyn', [], callback);
+      },
+      function(callback) {
+        client.query(
+          'DROP TEXT SEARCH DICTIONARY IF EXISTS adresser_xsyn_' + options.version + ' CASCADE;' +
+            'CREATE TEXT SEARCH DICTIONARY adresser_xsyn_' + options.version + '(' +
+            ' template=xsyn_template, rules=adresser_xsyn_' + options.version + ', matchsynonyms=true' +
+          ')', [], callback);
+      },
+      function(callback) {
+        client.query(
+          'DROP TEXT SEARCH DICTIONARY IF EXISTS adresser_unaccent_' + options.version + ' CASCADE;' +
+            'CREATE TEXT SEARCH DICTIONARY adresser_unaccent_' + options.version + '(' +
+            ' template=unaccent, rules=adresser_unaccent_' + options.version +
+            ')', [], callback);
+      },
+      function(callback) {
+        client.query(
+          'DROP TEXT SEARCH CONFIGURATION IF EXISTS adresser CASCADE;' +
+          'CREATE TEXT SEARCH CONFIGURATION adresser (copy=simple);' +
+          'ALTER TEXT SEARCH CONFIGURATION adresser ' +
+          'ALTER MAPPING FOR asciiword,word,numword,asciihword,hword,numhword ' +
+          'WITH adresser_unaccent_' + options.version + ', adresser_xsyn_' + options.version + ', simple;', [], callback);
+      },
+      sqlCommon.psqlScript(client, __dirname, 'reindex-search.sql'),
+      function(callback) {
+        commit(null, function(err) {
+          callback(err);
+        });
+      }
+    ], function(err) {
+      exitOnErr(err);
+      console.log('done!');
+    });
+  });
+});
