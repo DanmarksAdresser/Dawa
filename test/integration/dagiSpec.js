f69bd9dec4096ea28bd6994ca4fb938be4df5eab
Anders Hessellund Jensen <ahj@trifork.com>
2014-09-15 11:48:43 +0200

    Add entry to synonym dictionary. Add test for geo_version update

--- test/integration/dagiSpec.js
+++ test/integration/dagiSpec.js
@@ -52,27 +52,48 @@ describe('DAGI updates', function() {
     });
   });
 
-  it('When updating a DAGI tema, the adgangsadresser_temaer_matview table should be updated', function(done) {
-    dbapi.withRollbackTransaction(function(err, client, transactionDone) {
-      if(err) throw err;
-      dagi.addDagiTema(client, sampleTema, function(err, createdTemaId) {
+  describe('Update of DAGI tema', function() {
+    var transactionDone, updated, client, createdTemaId;
+
+    beforeEach(function(done) {
+      console.log('BEFORE EACH NESTED');
+      dbapi.withRollbackTransaction(function(err, _client, _transactionDone) {
         if(err) throw err;
-        var updated = _.clone(sampleTema);
-        updated.polygons=['POLYGON((' +
-          '725025.18 6166264.37,' +
-          '725025.18 6167400.76,' +
-          '731289.6 6167400.76,' +
-          '731289.6 6166264.37,' +
-          '725025.18 6166264.37))'];
-        dagi.updateDagiTema(client, updated, function(err) {
+        transactionDone = _transactionDone;
+        client = _client;
+        dagi.addDagiTema(client, sampleTema, function(err, _createdTemaId) {
+          createdTemaId = _createdTemaId;
           if(err) throw err;
-          client.query("select count(*) as c FROM adgangsadresser_temaer_matview WHERE tema = 'region' AND tema_id = $1", [createdTemaId], function(err, result) {
-            if(err) throw err;
-            transactionDone();
-            expect(result.rows[0].c).toBe('226');
-            done();
-          });
+          updated = _.clone(sampleTema);
+          updated.polygons=['POLYGON((' +
+            '725025.18 6166264.37,' +
+            '725025.18 6167400.76,' +
+            '731289.6 6167400.76,' +
+            '731289.6 6166264.37,' +
+            '725025.18 6166264.37))'];
+          dagi.updateDagiTema(client, updated, done);
         });
+      });
+    });
+
+    it('When updating a DAGI tema, the adgangsadresser_temaer_matview table should be updated', function(done) {
+      client.query(
+        "select count(*) as c FROM adgangsadresser_temaer_matview WHERE tema = 'region' AND tema_id = $1",
+        [createdTemaId],
+        function (err, result) {
+          if (err) throw err;
+          transactionDone();
+          expect(result.rows[0].c).toBe('226');
+          done();
+        });
+    });
+
+    it('When updating a tema, the geo_version should be updated', function(done) {
+      client.query("select geo_version from temaer where id = $1", [createdTemaId], function(err, result) {
+        if(err) throw err;
+        expect(result.rows[0].geo_version).toBe(2);
+        transactionDone();
+        done();
       });
     });
   });
