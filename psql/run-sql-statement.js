519be2442c85e5bbb5c6f2777028e9993a6a4670
Jakob Færch <jrf@trifork.com>
2014-11-18 14:48:15 +0100

    Refaktor ikke-cli drevet sql-udførsel fra run-sql-statement.js

--- psql/run-sql-statement.js
+++ psql/run-sql-statement.js
@@ -1,12 +1,9 @@
 "use strict";
 
-var async = require('async');
 var cli = require('cli');
-var fs = require('fs');
-var sqlCommon = require('./common');
-var _ = require('underscore');
-
 var cliParameterParsing = require('../bbr/common/cliParameterParsing');
+var run_sql_statement = require('./run-sql-statement-impl.js');
+var _ = require('underscore');
 
 var optionSpec = {
   pgConnectionUrl: [false, 'URL som anvendes ved forbindelse til databasen', 'string'],
@@ -27,53 +24,5 @@ cli.main(function(args, options) {
   cliParameterParsing.checkRequiredOptions(options, _.without(_.keys(optionSpec), 'disableTriggers'));
 
   var statement = args[0];
-  sqlCommon.withWriteTransaction(options.pgConnectionUrl, function(err, client, commit) {
-    exitOnErr(err);
-    client.on('error', function(err) {
-      exitOnErr(err);
-    });
-    client.on('notice', function(msg) {
-      console.log("notice: %j", msg);
-    });
-    client.query("SET client_min_messages='INFO'",[], function(err) {
-      exitOnErr(err);
-      async.series([
-        function(callback) {
-          client.query("set work_mem='500MB'; set maintenance_work_mem='500MB'", [], callback);
-        },
-        function(callback) {
-          if(options.disableTriggers) {
-            console.log('disabling triggers');
-            sqlCommon.disableTriggers(client)(callback);
-          }
-          else {
-            console.log('running script with triggers enabled');
-            callback();
-          }
-        },
-        function(callback) {
-          var commands = statement.split(';');
-          async.eachSeries(commands, function(command, callback) {
-            command = command.trim();
-            console.log('executing statement %s', command);
-            client.query(command, [], callback);
-          }, callback);
-        },
-        function(callback) {
-          if(options.disableTriggers) {
-            console.log('enabling triggers');
-            sqlCommon.enableTriggers(client)(callback);
-          }
-          else {
-            callback();
-          }
-        }
-        ],function(err) {
-          exitOnErr(err);
-          commit(null, function(err) {
-            exitOnErr(err);
-          });
-        });
-      });
-    });
+  run_sql_statement(statement, options.pgConnectionUrl, options.disableTriggers, exitOnErr);
 });
