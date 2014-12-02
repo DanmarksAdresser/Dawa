665531640d3056e5c3cf9301f3d259f1ff98ae54
Jakob Færch <jrf@trifork.com>
2014-12-02 13:30:59 +0100

    #341 Tilføj sammensat nøgle. Tilføj vejkode

--- apiSpecification/adgangsadresse/kvhTransformer.js
+++ apiSpecification/adgangsadresse/kvhTransformer.js
@@ -4,5 +4,6 @@ var kode4String = require('../util').kode4String;
 
 exports.format = function(rs) {
   return kode4String(rs.kommunekode || 0) +
-         ("0000" + rs.husnr).slice(-4);
+         kode4String(rs.vejkode || 0) +
+         kode4String(rs.husnr || 0);
 };
--- test/unit/kvhTransformerSpec.js
+++ test/unit/kvhTransformerSpec.js
@@ -6,14 +6,28 @@ describe('Formatting kvh from an adgangsadresse recordset', function() {
   it('should place kommunekode in in positions 0-3', function() {
     expect(transformer.format({kommunekode: 1234}).substring(0, 4)).toBe('1234');
   });
-  it('should prepend kommunekode with zeroes', function() {
+  it('should prepend kommunekode with zeroes to fill 4 characters', function() {
     expect(transformer.format({kommunekode: 5}).substring(0, 4)).toBe('0005');
   });
   it('should use four zeroes if no kommunekode', function() {
     expect(transformer.format({}).substring(0, 4)).toBe('0000');
   });
 
-  it('should place husnr in position 4-7', function() {
-    expect(transformer.format({husnr: '134B'}).substring(4, 8)).toBe('134B');
+  it('should place vejkode in position 4-7', function() {
+    expect(transformer.format({vejkode: 5678}).substring(4, 8)).toBe('5678');
   });
+
+  it('should prepend husnr with zeroes to fill 4 characters', function() {
+    expect(transformer.format({vejkode: 7}).substring(4, 8)).toBe('0007');
+  });
+
+  it('should place husnr in position 8-11', function() {
+    expect(transformer.format({husnr: '134B'}).substring(8, 12)).toBe('134B');
+  });
+
+  it('should prepend husnr with zeroes to fill 4 characters', function() {
+    expect(transformer.format({husnr: '13'}).substring(8, 12)).toBe('0013');
+  });
+
+
 });
