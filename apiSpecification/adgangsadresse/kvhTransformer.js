ee7c1a8ec6dffd0ed3865bd6e0c0f253f3c7af0e
Jakob Færch <jrf@trifork.com>
2014-12-02 18:20:31 +0100

    #341 Tilføj sammensat nøgle. Ideen om at lave links fra KVH(X)-felter til de DAWA datafelter de repræsenterer var ikke så god i praksis som jeg troede, så nu er den droppet.

--- apiSpecification/adgangsadresse/kvhTransformer.js
+++ apiSpecification/adgangsadresse/kvhTransformer.js
@@ -9,6 +9,6 @@ exports.format = function(rs) {
          pad(rs.husnr, 4);
 };
 
-exports.kvhFieldsDts = '<dt>Index 0-3: Kommunekode</dt><dd>Kommunekode for adressen (TODO her mangler link). Hvis kommunekoden har mindre end 4 cifre, foranstilles med nuller, kommunekode 175 repræsenteres altså fx som "0175". Hvis en adresse ikke har et tilknyttet kommunenr er værdien "0000"</dd>' +
-                       '<dt>Index 4-7: Vejstykkekode</dt><dd>Kode for adressens vejstykke (TODO her mangler link). Hvis vejstykkets kode har mindre end 4 cifre, foranstilles med nuller, vejstykkekode 370 repræsenteres altså fx som "0370".</dd>' +
-                       '<dt>Index 8-11: Husnr</dt><dd>Adressens husnr (inklusive evt. bogstav) (TODO her mangler link). Hvis husnummeret har mindre end 4 cifre, foranstilles med underscores, 1C repræsenteres altså fx som ‘__1C’. Husnumre uden bogstav repræsenteres blot med foranstillede underscores, så fx 17 repræsenteres som "__17".</dd>';
+exports.kvhFieldsDts = '<dt>Index 0-3: Kommunekode</dt><dd>Kommunekode for adressen. Hvis kommunekoden har mindre end 4 cifre, foranstilles med nuller, kommunekode 175 repræsenteres altså fx som "0175". Hvis en adresse ikke har et tilknyttet kommunenr er værdien "0000"</dd>' +
+                       '<dt>Index 4-7: Vejstykkekode</dt><dd>Kode for adressens vejstykke. Hvis vejstykkets kode har mindre end 4 cifre, foranstilles med nuller, vejstykkekode 370 repræsenteres altså fx som "0370".</dd>' +
+                       '<dt>Index 8-11: Husnr</dt><dd>Adressens husnr (inklusive evt. bogstav). Hvis husnummeret har mindre end 4 cifre, foranstilles med underscores, 1C repræsenteres altså fx som ‘__1C’. Husnumre uden bogstav repræsenteres blot med foranstillede underscores, så fx 17 repræsenteres som "__17".</dd>';
--- apiSpecification/adresse/kvhxTransformer.js
+++ apiSpecification/adresse/kvhxTransformer.js
@@ -10,5 +10,5 @@ exports.format = function(rs) {
 };
 
 exports.kvhxFieldsDts = kvhTransformer.kvhFieldsDts +
-'<dt>Index 12-14: Etage</dt><dd>Adressens etage (TODO her mangler link. Hvis etageangivelsen fylder mindre end 3 tegn, foranstilles med underscore, ‘st’ repræsenteres altså som "_st"</dd>' +
+'<dt>Index 12-14: Etage</dt><dd>Adressens etage. Hvis etageangivelsen fylder mindre end 3 tegn, foranstilles med underscore, ‘st’ repræsenteres altså som "_st"</dd>' +
 '<dt>Index 15-18: Dør</dt><dd>Angivelse af dør. Hvis dørbetegnelsen fylder mindre end 3 tegn, foranstilles med underscore, 2 repræsenteres altså som "___2"';
