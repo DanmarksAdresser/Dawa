44fbd2ef3eb057d6dd91eb1933df1a60b63de44f
Jakob Færch <jrf@trifork.com>
2014-12-05 10:08:07 +0100

    Ret eksempel - dørbetegnelse kan fylde 4

--- apiSpecification/adresse/kvhxTransformer.js
+++ apiSpecification/adresse/kvhxTransformer.js
@@ -11,4 +11,4 @@ exports.format = function(rs) {
 
 exports.kvhxFieldsDts = kvhTransformer.kvhFieldsDts +
 '<dt>Index 12-14: Etage</dt><dd>Adressens etage. Hvis etageangivelsen fylder mindre end 3 tegn, foranstilles med underscore, ‘st’ repræsenteres altså som "_st"</dd>' +
-'<dt>Index 15-18: Dør</dt><dd>Angivelse af dør. Hvis dørbetegnelsen fylder mindre end 3 tegn, foranstilles med underscore, 2 repræsenteres altså som "___2"';
+'<dt>Index 15-18: Dør</dt><dd>Angivelse af dør. Hvis dørbetegnelsen fylder mindre end 4 tegn, foranstilles med underscore, 2 repræsenteres altså som "___2"';
