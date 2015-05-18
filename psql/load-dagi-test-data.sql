d82431473a18b13de5a66d9a9058ec1eb0a5df12
Anders Hessellund Jensen <anders@andershessellund.dk>
2015-05-18 13:28:28 +0200

    Add some test data for storkredse

--- psql/load-dagi-test-data.sql
+++ psql/load-dagi-test-data.sql
@@ -122,5 +122,6 @@ VALUES
   ('zone',now(), now(), 1, '{ "zone":1 }', ST_GeomFromText('MULTIPOLYGON(((725020 6166300,725030 6166300,725030 6166310,725020 6166310,725020 6166300)))', 25832)),
   ('afstemningsomraade',now(), now(), 1, '{ "kode":99, "navn": "Afstemningsomraade test" }', ST_GeomFromText('MULTIPOLYGON(((725020 6166300,725030 6166300,725030 6166310,725020 6166310,725020 6166300)))', 25832)),
   ('valglandsdel',now(), now(), 1, '{ "bogstav":"A", "navn": "Valglandsdel test" }', ST_GeomFromText('MULTIPOLYGON(((725020 6166300,725030 6166300,725030 6166310,725020 6166310,725020 6166300)))', 25832)),
+  ('storkreds',now(), now(), 1, '{ "nummer":99, "navn": "Storkreds test" }', ST_GeomFromText('MULTIPOLYGON(((725020 6166300,725030 6166300,725030 6166310,725020 6166310,725020 6166300)))', 25832)),
   ('jordstykke',now(), now(), 1, '{ "ejerlavkode":1, "matrikelnr": "ab1f", "kommunekode": 99, "regionskode": 99, "sognekode": 99, "retskredskode": 99, "esrejendomsnr": 123456, "sfeejendomsnr":12345 }', ST_GeomFromText('MULTIPOLYGON(((725020 6166300,725030 6166300,725030 6166310,725020 6166310,725020 6166300)))', 25832));
 
