041f5f6048ee44a3f60c61429fce081e42698e69
Anders Hessellund Jensen <anders@andershessellund.dk>
2015-05-29 12:05:58 +0200

    Levenshtein ordering for adgangsadresse

--- apiSpecification/adgangsadresse/sqlModel.js
+++ apiSpecification/adgangsadresse/sqlModel.js
@@ -1,12 +1,15 @@
 "use strict";
 
 var columns = require('./columns');
+var dbapi = require('../../dbapi');
 var nameAndKey = require('./nameAndKey');
 var parameters = require('./parameters');
 var sqlParameterImpl = require('../common/sql/sqlParameterImpl');
 var sqlUtil = require('../common/sql/sqlUtil');
+var util = require('../util');
 
 var assembleSqlModel = sqlUtil.assembleSqlModel;
+var notNull = util.notNull;
 
 var baseQuery = function () {
   return {
@@ -18,13 +21,41 @@ var baseQuery = function () {
   };
 };
 
+function addAdditionalAdgangsadresseOrdering(sqlParts, rawQuery) {
+// we order according to levenshtein distance when the addresses have same rank.
+  // This improves ordering of addresses with identical rank, especially if
+  // several addresses matches 100%.
+  var rawQueryAlias = dbapi.addSqlParameter(sqlParts, rawQuery);
+  sqlParts.orderClauses.push("levenshtein(adressebetegnelse(vejnavn, husnr, NULL, NULL, supplerendebynavn, postnr::varchar, postnrnavn), " + rawQueryAlias + ") ASC");
+  sqlParts.orderClauses.push('husnr');
+}
+var searchAdgangsadresse = function(columnSpec) {
+  var searchFn = sqlParameterImpl.search(columnSpec);
+  return function(sqlParts, params) {
+    if(notNull(params.search)) {
+      searchFn(sqlParts, params);
+      addAdditionalAdgangsadresseOrdering(sqlParts, params.search);
+    }
+  };
+};
+
+var autocompleteAdgangsadresse = function(columnSpec) {
+  var autocompleteFn = sqlParameterImpl.autocomplete(columnSpec);
+  return function(sqlParts, params) {
+    if(notNull(params.autocomplete)) {
+      autocompleteFn(sqlParts, params);
+      addAdditionalAdgangsadresseOrdering(sqlParts, params.autocomplete);
+    }
+  };
+};
+
 var parameterImpls = [
   sqlParameterImpl.simplePropertyFilter(parameters.propertyFilter, columns),
   sqlParameterImpl.geomWithin(),
   sqlParameterImpl.reverseGeocoding(),
   sqlParameterImpl.dagiFilter(),
-  sqlParameterImpl.search(columns),
-  sqlParameterImpl.autocomplete(columns, ['husnr']),
+  searchAdgangsadresse(columns),
+  autocompleteAdgangsadresse(columns),
   sqlParameterImpl.paging(columns, nameAndKey.key)
 ];
 
