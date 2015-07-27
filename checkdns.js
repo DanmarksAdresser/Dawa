8fe7e09ac42227ceafb389dfaf57fe7298024e98
Anders Hessellund Jensen <anders@andershessellund.dk>
2015-07-27 12:52:33 +0200

    Fix checkdns to be compatible with 0.10

--- checkdns.js
+++ checkdns.js
@@ -5,7 +5,16 @@ var q = require('q');
 var _ = require('underscore');
 
 module.exports = function(hostname) {
-  return q.nfcall(dns.lookup, hostname, { family: "4"}).then(function(lookupResult) {
+  var options;
+  if(process.version.substring(0, 5) === 'v0.10') {
+    options = 4;
+  }
+  else {
+    options = {
+      family: 4
+    };
+  }
+  return q.nfcall(dns.lookup, hostname, options).then(function(lookupResult) {
     var lookupAddress = lookupResult[0];
     return q.nfcall(dns.resolve4, hostname).then(function(resolveAddresses) {
       return {
