01278473a9ff2992a699573649fe423859972cbe
Anders Hessellund Jensen <anders@andershessellund.dk>
2015-08-18 14:39:33 +0200

    Hvis der spørges efter hændelser på sekvensnumre vi endnu ikke har skal vi returnere en fejl

--- apiSpecification/replikering/events/sqlModels.js
+++ apiSpecification/replikering/events/sqlModels.js
@@ -7,6 +7,7 @@ var mappings = require('./../columnMappings');
 var sqlParameterImpl = require('../../common/sql/sqlParameterImpl');
 var sqlUtil = require('../../common/sql/sqlUtil');
 var parameters = require('./parameters');
+var querySenesteSekvensnummer = require('../sekvensnummer/querySenesteSekvensnummer');
 var temaer = require('../../temaer/temaer');
 
 function createSqlModel( columnMappings , simpleFilterParameters, baseQuery) {
@@ -15,36 +16,42 @@ function createSqlModel( columnMappings , simpleFilterParameters, baseQuery) {
       return ['sekvensnummer', 'operation', 'tidspunkt'].concat(_.pluck(columnMappings, 'name'));
     },
     stream: function (client, fieldNames, params, callback) {
-      var query = baseQuery();
-      if (params.sekvensnummerfra) {
-        var fromAlias = dbapi.addSqlParameter(query, params.sekvensnummerfra);
-        dbapi.addWhereClause(query, 'h.sequence_number >= ' + fromAlias);
-      }
-      if (params.sekvensnummertil) {
-        var toAlias = dbapi.addSqlParameter(query, params.sekvensnummertil);
-        dbapi.addWhereClause(query, 'h.sequence_number <= ' + toAlias);
-      }
-      if (params.tidspunktfra) {
-        var timeFromAlias = dbapi.addSqlParameter(query, params.tidspunktfra);
-        dbapi.addWhereClause(query, 'h.time >=' + timeFromAlias);
-      }
+      return querySenesteSekvensnummer(client).then(function(senesteHaendelse) {
+        if (params.sekvensnummertil && senesteHaendelse.sekvensnummer < params.sekvensnummertil) {
+          throw new sqlUtil.InvalidParametersError("Hændelse med sekvensnummer " + params.sekvensnummertil + " findes ikke. Seneste sekvensnummer: " + senesteHaendelse.sekvensnummer);
+        }
+        var query = baseQuery();
+        if (params.sekvensnummerfra) {
+          var fromAlias = dbapi.addSqlParameter(query, params.sekvensnummerfra);
+          dbapi.addWhereClause(query, 'h.sequence_number >= ' + fromAlias);
+        }
+        if (params.sekvensnummertil) {
+          var toAlias = dbapi.addSqlParameter(query, params.sekvensnummertil);
+          dbapi.addWhereClause(query, 'h.sequence_number <= ' + toAlias);
+        }
+        if (params.tidspunktfra) {
+          var timeFromAlias = dbapi.addSqlParameter(query, params.tidspunktfra);
+          dbapi.addWhereClause(query, 'h.time >=' + timeFromAlias);
+        }
 
-      if (params.tidspunkttil) {
-        var timeToAlias = dbapi.addSqlParameter(query, params.tidspunkttil);
-        dbapi.addWhereClause(query, 'h.time <=' + timeToAlias);
-      }
-      // we want to be able to find events for a specific ID.
-      var keyColumns = _.reduce(columnMappings, function (memo, mapping) {
-        var columnName = mapping.column || mapping.name;
-        memo[mapping.name] = {
-          where: columnName
-        };
-        return memo;
-      }, {});
-      var propertyFilter = sqlParameterImpl.simplePropertyFilter(simpleFilterParameters, keyColumns);
-      propertyFilter(query, params);
-      var dbQuery = dbapi.createQuery(query);
-      dbapi.streamRaw(client, dbQuery.sql, dbQuery.params, callback);
+        if (params.tidspunkttil) {
+          var timeToAlias = dbapi.addSqlParameter(query, params.tidspunkttil);
+          dbapi.addWhereClause(query, 'h.time <=' + timeToAlias);
+        }
+        // we want to be able to find events for a specific ID.
+        var keyColumns = _.reduce(columnMappings, function (memo, mapping) {
+          var columnName = mapping.column || mapping.name;
+          memo[mapping.name] = {
+            where: columnName
+          };
+          return memo;
+        }, {});
+        var propertyFilter = sqlParameterImpl.simplePropertyFilter(simpleFilterParameters, keyColumns);
+        propertyFilter(query, params);
+        var dbQuery = dbapi.createQuery(query);
+
+        return dbapi.streamRaw(client, dbQuery.sql, dbQuery.params);
+      }).nodeify(callback);
     }
   };
 }
