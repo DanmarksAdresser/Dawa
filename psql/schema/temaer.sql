2f69ad90e98086edf9695eb2da2ce24abd57aeb8
Anders Hessellund Jensen <anders@andershessellund.dk>
2015-08-24 16:34:11 +0200

    Temaer skal også indeksere kode

--- psql/schema/temaer.sql
+++ psql/schema/temaer.sql
@@ -3,7 +3,7 @@ DROP FUNCTION IF EXISTS temaer_init() CASCADE;
 CREATE FUNCTION temaer_init() RETURNS void
 LANGUAGE sql AS
 $$
-UPDATE temaer SET tsv = to_tsvector('adresser',(fields->>'navn')) WHERE false;
+UPDATE temaer SET tsv = to_tsvector('adresser',coalesce(temaer.fields->>'kode', '') || ' ' || coalesce(temaer.fields->>'navn')) WHERE temaer.tema <> 'jordstykke';
 $$;
 
 -- Trigger which maintains the tsv column
@@ -11,7 +11,7 @@ DROP FUNCTION IF EXISTS temaer_tsv_update() CASCADE;
 CREATE OR REPLACE FUNCTION temaer_tsv_update()
   RETURNS TRIGGER AS $$
 BEGIN
-  NEW.tsv = to_tsvector('adresser', coalesce(NEW.fields->>'navn', ''));
+  NEW.tsv = to_tsvector('adresser', coalesce(NEW.fields->>'kode', '') || ' ' || coalesce(NEW.fields->>'navn', ''));
   RETURN NEW;
 END;
 $$ LANGUAGE PLPGSQL;
