b14aa70a1b818f1dab0d1563b0a322bea2b95e97
Anders Hessellund Jensen <anders@andershessellund.dk>
2017-02-28 13:54:43 +0100

    perf test fixes

--- performance-tests/generators/btm-test.js
+++ performance-tests/generators/btm-test.js
@@ -2,12 +2,7 @@ module.exports = [{
   name: 'adresseudtræk',
   type: 'fixedConcurrency',
   url: '/adresser',
-  concurrency: 5,
-  rampUpDelay: 5000,
-}, {
-  name: 'jordstykkeudtræk',
-  type: 'fixedConcurrency',
-  url: '/jordstykker',
-  concurrency: 5,
-  rampUpDelay: 5000,
+  concurrency: 50,
+  clientCount: 50,
+  rampUpDelay: 100,
 }];
