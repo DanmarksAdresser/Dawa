62b24705e776276b6556809ddcedb9c444a9ccbd
Anders Hessellund Jensen <anders@andershessellund.dk>
2017-03-02 09:56:11 +0100

    Fix fejl i datavask

--- psql/schema/vask_adresser_unikke.sql
+++ psql/schema/vask_adresser_unikke.sql
@@ -3,7 +3,7 @@ CREATE FUNCTION vask_adresser_unikke_update_tsv() RETURNS void
 LANGUAGE sql AS
 $$
 UPDATE vask_adresser_unikke SET tsv = (
-  setweight(to_tsvector('adresser', vejnavn || ' ' || (husnr).tal || (husnr).bogstav), 'A') ||
+  setweight(to_tsvector('adresser', processForIndexing(vejnavn) || ' ' || (husnr).tal || (husnr).bogstav), 'A') ||
   setweight(to_tsvector('adresser', processforindexing(COALESCE(etage, '') ||' ' || COALESCE(doer, ''))), 'B') ||
   setweight(to_tsvector('adresser', processForIndexing(COALESCE(supplerendebynavn, ''))), 'C') ||
   setweight(to_tsvector('adresser', to_char(postnr, 'FM0000') || ' ' || postnrnavn), 'D')
