be2405124ad036715ad991fce843acc51890e718
Anders Hessellund Jensen <anders@andershessellund.dk>
2017-11-06 09:57:50 +0100

    Mere stednavn-test

--- test/integration/importStednavneImplSpec.js
+++ test/integration/importStednavneImplSpec.js
@@ -4,13 +4,17 @@ const assert = require('chai').assert;
 const path = require('path');
 const {go} = require('ts-csp');
 
+const helpers = require('./helpers');
 const importStednavneImpl = require('../../stednavne/importStednavneImpl');
 const {withImportTransaction} = require('../../importUtil/importUtil');
 const testdb = require('../helpers/testdb2');
+const registry = require('../../apiSpecification/registry');
+require('../../apiSpecification/allSpecs');
+
 
 describe('Import af stednavne', () => {
   testdb.withTransactionAll('empty', clientFn => {
-    it('Kan importere en initiel stednavnefil', () => go(function*() {
+    it('Kan importere en initiel stednavnefil', () => go(function* () {
       const client = clientFn();
       yield withImportTransaction(client, 'importStednavne',
         txid => importStednavneImpl.importStednavne(
@@ -33,13 +37,13 @@ describe('Import af stednavne', () => {
       assert.strictEqual(stednavn.geo_version, 1);
     }));
 
-    it('Har udfyldt stednavne_divided', () => go(function*() {
+    it('Har udfyldt stednavne_divided', () => go(function* () {
       const rows = (yield clientFn().query('select * from stednavne_divided')).rows;
       assert.strictEqual(rows.length, 2);
     }));
 
 
-    it('Kan opdatere stednavn', () => go(function*() {
+    it('Kan opdatere stednavn', () => go(function* () {
       const client = clientFn();
       yield withImportTransaction(client, 'importStednavne',
         txid => importStednavneImpl.importStednavne(
@@ -60,9 +64,91 @@ describe('Import af stednavne', () => {
       assert.strictEqual(stednavn.geo_version, 2);
     }));
 
-    it('Har opdateret stednavne_divided', () => go(function*() {
-      const rows = (yield clientFn().query('select * from stednavne_divided')).rows;
+    it('Har opdateret stednavne_divided', () => go(function* () {
+      const rows = yield clientFn().queryRows('select * from stednavne_divided');
       assert.strictEqual(rows.length, 1);
+    }));
+
+  });
+
+  testdb.withTransactionEach('test', clientFn => {
+    const queryResource = registry.get({
+      entityName: 'stednavn',
+      type: 'resource',
+      qualifier: 'query'
+    });
+    const autocompleteResource = registry.get({
+      entityName: 'stednavn',
+      type: 'resource',
+      qualifier: 'autocomplete'
+    });
+
+    it('Stednavn er udfyldt med kommune', () => go(function*() {
+      const result = yield helpers.getJson(clientFn(), queryResource, {}, {
+        id: '19e4392f-c7b1-5d41-0000-d380220a2006'
+      });
+      assert.strictEqual(result[0].kommuner.length, 1);
+    }));
+
+    it('Kan søge med autocomplete', () => go(function*() {
+      const result = yield helpers.getJson(clientFn(), autocompleteResource, {}, {
+        q: 'test'
+      });
+      assert.strictEqual(result.length, 1);
+      const match = result[0];
+      assert.strictEqual(match.navn, 'Test stednavn punkt');
+    }));
+
+    it('Kan finde stednavne med fuzzy søgning', () => go(function*() {
+      const result = yield helpers.getJson(clientFn(), queryResource, {}, {
+        q: 'tst stenan pkt',
+        fuzzy: 'true'
+      });
+      assert(result.length > 0);
+      const match = result[0];
+      assert.strictEqual(match.navn, 'Test stednavn punkt');
+    }));
+
+    it('Kan hente stednavn i geojson og srid 25832', () => go(function*() {
+      const result = yield helpers.getJson(clientFn(), queryResource, {}, {
+        id: '19e4392f-c7b1-5d41-0000-d380220a2006',
+        format: 'geojson',
+        srid: '25832'
+      });
+      const feature = result.features[0];
+      assert.strictEqual(feature.geometry.coordinates[0], 725021);
+      assert.strictEqual(feature.geometry.coordinates[1], 6166301);
+
+
+      assert.strictEqual(feature.properties.visueltcenter_x, 725021);
+      assert.strictEqual(feature.properties.visueltcenter_y, 6166301);
+    }));
+
+    it('Kan finde adgangsadresser ud fra stednavn id', () => go(function*() {
+      const adgadrResource = registry.get({
+        entityName: 'adgangsadresse',
+        type: 'resource',
+        qualifier: 'query'
+      });
+      const result = yield helpers.getJson(clientFn(), adgadrResource, {}, {
+        stednavnid: '12337669-8e23-6b98-e053-d480220a5a3f',
+        struktur: 'mini'
+      });
+      assert.strictEqual(result.length, 2);
+    }));
+
+    it('Kan finde adgangsadresser indenfor en bestemt afstand fra stednavn', () => go(function*() {
+      const adgadrResource = registry.get({
+        entityName: 'adgangsadresse',
+        type: 'resource',
+        qualifier: 'query'
+      });
+      const result = yield helpers.getJson(clientFn(), adgadrResource, {}, {
+        stednavnid: '12337669-8e23-6b98-e053-d480220a5a3f',
+        struktur: 'mini',
+        stednavnafstand: '1000'
+      });
+      assert.strictEqual(result.length, 3);
     }));
   });
 });
