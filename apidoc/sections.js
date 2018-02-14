db3ec103cdba9b5684a95419d6d4ea69d96cf564
Anders Hessellund Jensen <anders@andershessellund.dk>
2018-02-14 09:22:54 +0100

    Bebyggelser med på API-oversigt

--- apidoc/sections.js
+++ apidoc/sections.js
@@ -22,7 +22,7 @@ module.exports = [{
   name: 'Stednavne',
   headingClass: 'h2-icon h2-sted',
   lead: 'For en generel introduktion til stednavne, se <a href="/dok/stednavne">her<a/>.',
-  entities: [ 'stednavn', 'stednavntype']
+  entities: [ 'stednavn', 'stednavntype', 'bebyggelse']
 }, {
   name: 'Andet',
   entities: ['senesteSekvensnummer', 'zone']
