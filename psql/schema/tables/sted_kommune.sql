b000aadb0970f00e6676ae352d6117358e243056
Anders Hessellund Jensen <anders@andershessellund.dk>
2018-04-17 13:51:42 +0200

    Fix sted_kommune view

--- psql/schema/tables/sted_kommune.sql
+++ psql/schema/tables/sted_kommune.sql
@@ -1,7 +1,7 @@
 DROP MATERIALIZED VIEW IF EXISTS sted_kommune;
 CREATE MATERIALIZED VIEW sted_kommune AS
   (SELECT distinct s.id as stedid, k.kode as kommunekode
-  FROM stednavne_divided s JOIN kommuner_divided k ON  st_dwithin(s.geom, k.geom, 0));
+  FROM steder_divided s JOIN kommuner_divided k ON  st_dwithin(s.geom, k.geom, 0));
 
 CREATE UNIQUE INDEX ON sted_kommune(stedid, kommunekode);
 CREATE  INDEX ON sted_kommune(kommunekode);
