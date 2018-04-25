2102bb7d83886e02c428ceda6352a21042ded14d
Anders Hessellund Jensen <anders@andershessellund.dk>
2018-04-25 09:48:59 +0200

    Indledning til stednavne2

--- apidoc/stednavn2-page.js
+++ apidoc/stednavn2-page.js
@@ -2,9 +2,16 @@ module.exports = {
   entity: 'stednavn2',
   heading: 'Stednavne',
   lead: `API'et udstiller stednavne fra stednavneregisteret.`,
-  body: `<p>Stednavne navngiver <a href="/dok/api/sted">steder.</a>. Hvert sted har et
+  body: `
+<p>Stednavne navngiver <a href="/dok/api/sted">steder</a>. Hvert sted har et
 primært og 0 eller flere sekundære navne. Ved søgning i stednavne kan der således
-returneres mere end ét navn pr. sted.</p>`,
+returneres mere end ét navn pr. sted.</p>
+<p>Dette er version 2 af stednavne-API'et. I denne version af API'et udstilles stednavne og steder
+separat, da hvert sted kan have flere navne tilknyttet.</p>
+<p>Det er primært ved søgning ud fra navn, at det giver mening at anvende stednavne-API'et.
+Såfremt søgning sker ud fra andre egenskaber, så som type eller geografi, er det mere hensigtsmæssigt at anvende <a href="/dok/api/sted">sted-API'et</a>.
+Årsagen er, at ved søgning ud fra andre egenskaber end navne er det ikke hensigtsmæssigt at steder kan optræde mere end én gang
+i søgeresultatet.</p>`,
   sections: [
     {
       type: 'endpoint',
