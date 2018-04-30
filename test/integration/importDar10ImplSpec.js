68283016aafef17fc79416ae9a27ee9754c02ea0
Anders Hessellund Jensen <anders@andershessellund.dk>
2018-04-30 13:56:10 +0200

    Test af konvertering af oprettet/ændret for DAR-adresser til DAWA

--- test/integration/importDar10ImplSpec.js
+++ test/integration/importDar10ImplSpec.js
@@ -475,6 +475,113 @@ describe('Import af changesets', function() {
       assert.strictEqual(historyRow.virkningfra, "2015-11-12T23:14:55.760Z");
       assert.isNull(historyRow.virkningtil);
     }));
+
+    it('Mapning af Adresse til DAWA',()=> go(function*() {
+      const client = clientFn();
+      const firstHusnummer = {
+        "eventopret": 1,
+        "eventopdater": null,
+        "rowkey": 9,
+        "id": "0437a3ca-9886-48ea-90c8-504b68d179e6",
+        "registreringfra":  "2015-11-12T23:14:55.760Z",
+        "registreringtil": null,
+        "virkningfra":  "2015-11-12T23:14:55.760Z",
+        "virkningtil": null,
+        "status": "3",
+        "adgangsadressebetegnelse": "Sø Svenstrup Byvej 25, 4130 Viby Sjælland",
+        "adgangspunkt_id": "0a3f5081-d7c2-32b8-e044-0003ba298018",
+        "darafstemningsområde_id": "bee5d5a8-2e16-473d-851d-61b59079dd4b",
+        "darkommune_id": "e604599c-1cb8-4bf8-a78a-594ae303ee03",
+        "darmenighedsrådsafstemningsområde_id": "8e238ea8-9ab1-4de8-b6e1-bfb7d5180eba",
+        "darsogneinddeling_id": "a161a121-92af-494e-9280-27eeba3c952d",
+        "fk_bbr_bygning_adgangtilbygning": null,
+        "fk_bbr_tekniskanlæg_adgangtiltekniskanlæg": null,
+        "fk_geodk_bygning_geodanmarkbygning": "1023025565",
+        "fk_geodk_vejmidte_vejmidte": "1023123684",
+        "fk_mu_jordstykke_foreløbigtplaceretpåjordstykke": null,
+        "fk_mu_jordstykke_jordstykke": "2221014",
+        "husnummerretning": "POINT (-0.896849582121619 0.442335649759634)",
+        "husnummertekst": "25",
+        "navngivenvej_id": "e17d4273-ad3d-496f-85b1-73bba8b1a707",
+        "postnummer_id": "873e4c91-c2d3-4674-a491-2f0e30bac7eb",
+        "supplerendebynavn_id": null,
+        "vejpunkt_id": "79740068-fbe9-11e5-a32f-063320a53a26"
+      }
+      const firstAdresse = {
+        "eventopret": 1,
+        "eventopdater": null,
+        "rowkey": 10,
+        "id": "59b174ea-d060-4838-a238-00011004bc44",
+        "registreringfra":  "2015-11-12T23:14:55.76Z",
+        "registreringtil": null,
+        "virkningfra": "2015-11-12T23:14:55.76Z",
+        "virkningtil": null,
+        "status": "3",
+        "adressebetegnelse": "Okavangovej 8, Vinge, 3600 Frederikssund",
+        "dørbetegnelse": "",
+        "dørpunkt_id": null,
+        "etagebetegnelse": "",
+        "fk_bbr_bygning_bygning": null,
+        "husnummer_id": "0437a3ca-9886-48ea-90c8-504b68d179e6"
+      };
+      const firstChangeset = {
+        Adresse: [Object.assign({}, firstAdresse)],
+        Husnummer: [Object.assign({}, firstHusnummer)]
+      };
+
+      yield importDarImpl.withDar1Transaction(client, 'api', q.async(function*() {
+        yield withImportTransaction(client, 'importDar', (txid) => go(function*() {
+          yield importDarImpl.importChangeset(client, txid, firstChangeset);
+        }));
+      }));
+
+      const firstResult = yield client.queryRows('select * from enhedsadresser');
+      assert.strictEqual(firstResult.length, 1);
+      const firstRow = firstResult[0];
+      assert.strictEqual(firstRow.oprettet, '2015-11-13T00:14:55.76');
+      assert.strictEqual(firstRow.aendret, '2015-11-13T00:14:55.76');
+      assert.strictEqual(firstRow.objekttype, 1);
+
+      const change = [
+        Object.assign({}, firstAdresse, {
+          eventopdater: 2,
+          registreringtil: '2016-11-12T23:14:55.76Z'
+        }),
+        Object.assign({}, firstAdresse, {
+          eventopret: 2,
+          rowkey: 11,
+          registreringfra: '2016-11-12T23:14:55.76Z',
+          virkningtil: '2016-11-12T23:14:55.76Z'
+        }),
+        Object.assign({}, firstAdresse, {
+          eventopret: 2,
+          rowkey: 12,
+          registreringfra: '2016-11-12T23:14:55.76Z',
+          virkningfra: '2016-11-12T23:14:55.76Z',
+          etagebetegnelse: 'st',
+          dørbetegnelse: 'tv'
+        })];
+
+      const secondChangeset = {
+        Adresse: change
+      };
+
+      yield importDarImpl.withDar1Transaction(client, 'api', q.async(function*() {
+        yield withImportTransaction(client, 'importDar', (txid) => go(function*() {
+          yield importDarImpl.importChangeset(client, txid, secondChangeset);
+        }));
+      }));
+
+      const result2 = yield client.queryRows('select * from enhedsadresser');
+      // selvom der nu er to rækker i den originale DAR historik, bør vores egen komprimerede
+      // historik kun indeholde én, da alle andre felter end adressebetegnelse er identiske.
+      assert.strictEqual(result2.length, 1);
+      const row2 = result2[0];
+      assert.strictEqual(row2.oprettet, '2015-11-13T00:14:55.76');
+      assert.strictEqual(row2.aendret, '2016-11-13T00:14:55.76');
+      assert.strictEqual(row2.etage, 'st');
+      assert.strictEqual(row2.doer, 'tv');
+    }));
   });
 
 });
