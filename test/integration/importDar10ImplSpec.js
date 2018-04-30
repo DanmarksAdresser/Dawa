4c1fab993235575d627e84f799c8ade55c1fd96f
Anders Hessellund Jensen <anders@andershessellund.dk>
2018-04-30 13:27:55 +0200

    Test af historik-komprimering

--- test/integration/importDar10ImplSpec.js
+++ test/integration/importDar10ImplSpec.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const _ = require('underscore');
-const expect = require('chai').expect;
+const {expect, assert} = require('chai');
 const uuid = require('uuid');
 
 const moment = require('moment');
@@ -28,7 +28,7 @@ describe('DAR 1.0 tablemodels', () => {
     const column = _.findWhere(dar10TableModels.rawTableModels.NavngivenVej.columns, {name: 'vejnavnebeliggenhed_vejnavnelinje'});
     expect(column.distinctClause).to.exist;
   });
-})
+});
 
 describe('Import af DAR 1.0 udtræk', function () {
   this.timeout(60000);
@@ -400,5 +400,81 @@ describe('Import af changesets', function() {
       updatedAddress = (yield client.queryp('select * from adgangsadresser')).rows[0];
       expect(updatedAddress.postnr).to.equal(5000);
     }));
+
+    it('Historik bliver komprimeret', () => go(function* () {
+      const client = clientFn();
+      const firstAdresse = {
+        "eventopret": 1,
+        "eventopdater": null,
+        "rowkey": 10,
+        "id": "59b174ea-d060-4838-a238-00011004bc44",
+        "registreringfra":  "2015-11-12T23:14:55.76Z",
+        "registreringtil": null,
+        "virkningfra": "2015-11-12T23:14:55.76Z",
+        "virkningtil": null,
+        "status": "3",
+        "adressebetegnelse": "Okavangovej 8, Vinge, 3600 Frederikssund",
+        "dørbetegnelse": "",
+        "dørpunkt_id": null,
+        "etagebetegnelse": "",
+        "fk_bbr_bygning_bygning": null,
+        "husnummer_id": "0437a3ca-9886-48ea-90c8-504b68d179e6"
+      };
+      const firstChangeset = {
+        Adresse: [Object.assign({}, firstAdresse)]
+      };
+
+      yield importDarImpl.withDar1Transaction(client, 'api', q.async(function*() {
+        yield withImportTransaction(client, 'importDar', (txid) => go(function*() {
+          yield importDarImpl.importChangeset(client, txid, firstChangeset);
+        }));
+      }));
+
+      const result = yield client.queryRows('select rowkey, lower(virkning) as virkningfra, upper(virkning) as virkningtil  from dar1_adresse_history');
+      assert.strictEqual(result.length, 1);
+      assert.strictEqual(result[0].virkningfra, "2015-11-12T23:14:55.760Z");
+      assert.strictEqual(result[0].virkningtil, null);
+      const initialRowkey = result[0].rowkey;
+
+      // Vi laver en ændring hvor adressebegnelsen er ændret. Den bør komprimeres væk ved import.
+      const change = [
+        Object.assign({}, firstAdresse, {
+          eventopdater: 2,
+          registreringtil: '2016-11-12T23:14:55.76Z'
+        }),
+        Object.assign({}, firstAdresse, {
+          eventopret: 2,
+          rowkey: 11,
+          registreringfra: '2016-11-12T23:14:55.76Z',
+          virkningtil: '2016-11-12T23:14:55.76Z'
+        }),
+        Object.assign({}, firstAdresse, {
+          eventopret: 2,
+          rowkey: 12,
+          registreringfra: '2016-11-12T23:14:55.76Z',
+          virkningfra: '2016-11-12T23:14:55.76Z',
+          adressebetegnelse: "Okavangovej OPDATERET 8, Vinge, 3600 Frederikssund"
+        })];
+
+      const secondChangeset = {
+        Adresse: change
+      };
+
+      yield importDarImpl.withDar1Transaction(client, 'api', q.async(function*() {
+        yield withImportTransaction(client, 'importDar', (txid) => go(function*() {
+          yield importDarImpl.importChangeset(client, txid, secondChangeset);
+        }));
+      }));
+
+      const result2 = yield client.queryRows('select rowkey, lower(virkning) as virkningfra, upper(virkning) as virkningtil from dar1_adresse_history');
+      // selvom der nu er to rækker i den originale DAR historik, bør vores egen komprimerede
+      // historik kun indeholde én, da alle andre felter end adressebetegnelse er identiske.
+      assert.strictEqual(result2.length, 1);
+      const historyRow = result2[0];
+      assert.strictEqual(historyRow.rowkey, initialRowkey);
+      assert.strictEqual(historyRow.virkningfra, "2015-11-12T23:14:55.760Z");
+      assert.isNull(historyRow.virkningtil);
+    }));
   });
+
 });
