75ff1af8abe307dbd504744800de61dff51e4253
Anders Hessellund Jensen <anders@andershessellund.dk>
2018-05-14 07:39:18 +0200

    Fix til tilknytnings-historik

--- importUtil/tableDiffNg.js
+++ importUtil/tableDiffNg.js
@@ -5,6 +5,7 @@ const _ = require('underscore');
 
 const {allColumnNames, nonPrimaryColumnNames, publicColumnNames, publicNonKeyColumnNames, deriveColumnsForChange, makeSelectClause, derivedColumnNames, nonDerivedColumnNames, assignSequenceNumbers} = require('./tableModelUtil');
 const {selectList, columnsEqualClause} = require('../darImport/sqlUtil');
+const tableSchema = require('../psql/tableModel');
 
 const columnsDistinctClause = (tableA, tableB, columns) =>
   '(' + columns.map(column => column.distinctClause ?
@@ -232,7 +233,7 @@ const migrateInserts = (client, txid, tableModel) => {
                    valid_from,
                    ${selectFields}
                  FROM ${historyTableName} h
-                 join transaction_history th on h.valid_from = th.sequence_number
+                 left join transaction_history th on h.valid_from = th.sequence_number
                   WHERE valid_from IS NULL OR NOT EXISTS
                  (SELECT *
                   FROM ${historyTableName} h2
@@ -391,6 +392,85 @@ const assignSequenceNumbersToDependentTables = (client, txid, orderedTableModels
   }
 });
 
+const validateHistoryTable = (client, tableModel) => go(function* () {
+  const tableName = tableModel.table;
+  const report = {};
+  const subselect =
+    `SELECT *, row_number()
+  OVER (PARTITION BY ${tableModel.primaryKey.join(', ')}
+    ORDER BY txid desc nulls last, changeid desc NULLS LAST) as row_num
+FROM ${tableName}_changes`;
+  const currentQuery = `
+  select ${tableModel.columns.map(col => col.name).join(',')}
+  FROM (${subselect}) t
+  WHERE row_num=1 and operation <> 'delete'`;
+
+  const invalidCurrentCountQuery = `
+  WITH c AS (${currentQuery})
+  SELECT COUNT(*)::integer as invalid_count
+   FROM c FULL JOIN ${tableModel.table} t
+   ON ${columnsEqualClause('c', 't', tableModel.primaryKey)}
+   WHERE ${columnsDistinctClause('c', 't', tableModel.columns)}`;
+  report.invalidCurrentCount = (yield client.queryRows(invalidCurrentCountQuery))[0].invalid_count;
+
+  const invalidInsertsQuery =`
+  select c1.txid, count(*)::integer as invalid_count
+  FROM ${tableModel.table}_changes c1
+  LEFT JOIN LATERAL (SELECT * from ${tableModel.table}_changes c2
+  where ${columnsEqualClause('c1', 'c2', tableModel.primaryKey)} 
+  and (c2.txid < c1.txid or ((c2.changeid is null and c1.changeid is not null) or (c2.changeid < c1.changeid)))
+  order by txid desc nulls last, changeid desc nulls last
+  limit 1) c2
+  ON true
+  where c1.operation = 'insert' and c2.operation <> 'delete'
+  group by c1.txid`;
+  report.invalidInserts = (yield client.queryRows(invalidInsertsQuery));
+  const invalidUpdatesQuery =`
+  select c1.txid, count(*)::integer as invalid_count
+  FROM ${tableModel.table}_changes c1
+  LEFT JOIN LATERAL (SELECT * from ${tableModel.table}_changes c2
+  where ${columnsEqualClause('c1', 'c2', tableModel.primaryKey)} 
+  and (c2.txid < c1.txid or ((c2.changeid is null and c1.changeid is not null) or (c2.changeid < c1.changeid)))
+  order by txid desc nulls last, changeid desc nulls last
+  limit 1) c2
+  ON true
+  where c1.operation = 'update' and (c2.operation is null or c2.operation = 'delete')
+  group by c1.txid`;
+  report.invalidUpdates = (yield client.queryRows(invalidUpdatesQuery));
+  const invalidDeletesQuery =`
+  select c1.txid, count(*)::integer as invalid_count
+  FROM ${tableModel.table}_changes c1
+  LEFT JOIN LATERAL (SELECT * from ${tableModel.table}_changes c2
+  where ${columnsEqualClause('c1', 'c2', tableModel.primaryKey)} 
+  and (c2.txid < c1.txid or ((c2.changeid is null and c1.changeid is not null) or (c2.changeid < c1.changeid)))
+  order by txid desc nulls last, changeid desc nulls last
+  limit 1) c2
+  ON true
+  where c1.operation = 'delete' and(c2.operation is null or c2.operation = 'delete')
+  group by c1.txid`;
+  report.invalidDeletes = (yield client.queryRows(invalidDeletesQuery));
+  const invalidDuplicatesSubquery = `
+  select txid, changeid, ${tableModel.primaryKey.join(',')}, count(*)
+  from ${tableModel.table}_changes group by txid,changeid, ${tableModel.primaryKey.join(',')} having count(*) > 1`;
+  report.invalidDuplicates = (yield client.queryRows(`
+  select txid,count(*)::integer as invalid_count from (${invalidDuplicatesSubquery}) t group by txid order by txid`));
+  return report;
+});
+
+const validateHistoryImpl = (client, tableName, columnName, aggregate) => go(function*() {
+  const tableModels = tableName ? [tableSchema.tables[tableName]] : Object.values(tableSchema.tables);
+  const report = {};
+  for(let tableModel of tableModels) {
+    const existRows = yield client.queryRows(`SELECT (EXISTS (
+   SELECT 1 FROM information_schema.tables WHERE table_name = '${tableModel.table.toLowerCase()}_changes')) as exist`);
+    const exists = existRows[0].exist;
+    if(exists) {
+      report[tableModel.table] = yield validateHistoryTable(client, tableModel);
+    }
+  }
+  return report;
+});
+
 module.exports = {
   computeInserts,
   computeInsertsSubset,
@@ -415,5 +495,6 @@ module.exports = {
   del,
   getChangeTableSql,
   assignSequenceNumbersToDependentTables,
-  initializeChangeTable
+  initializeChangeTable,
+  validateHistoryImpl
 };
--- importUtil/validateHistory.js
+++ importUtil/validateHistory.js
@@ -3,19 +3,18 @@
 const {go} = require('ts-csp');
 const _ = require('underscore');
 
-const {runImporter} = require('../importUtil/runImporter');
+const {runImporter} = require('./runImporter');
 const proddb = require('../psql/proddb');
-const inspect = require('./inspectImpl');
+const { validateHistoryImpl } = require('./tableDiffNg');
 
 const optionSpec = {
   pgConnectionUrl: [false, 'URL som anvendes ved forbindelse til databasen', 'string'],
-  txid: [false, 'Transaktions-id', 'number'],
   tableName: [false, 'Inspicer enkelt tabel', 'string'],
   columnName: [false, 'Inspicer enkelt kolonne i tabel', 'string'],
   aggregate: [false, 'Aggreger ændringer', 'boolean', false]
 };
 
-runImporter('inspect', optionSpec, _.without(_.keys(optionSpec), 'tableName', 'columnName'), function (args, options) {
+runImporter('inspectHistory', optionSpec, _.without(_.keys(optionSpec), 'tableName', 'columnName'), function (args, options) {
   proddb.init({
     connString: options.pgConnectionUrl,
     pooled: false
@@ -23,6 +22,6 @@ runImporter('inspect', optionSpec, _.without(_.keys(optionSpec), 'tableName', 'c
 
   return proddb.withTransaction('READ_ONLY', client => go(function*() {
     /* eslint no-console: 0 */
-    console.log(JSON.stringify(yield inspect(client, options.txid, options.tableName, options.columnName, options.aggregate), null, 2));
+    console.log(JSON.stringify(yield validateHistoryImpl(client, options.tableName, options.columnName, options.aggregate), null, 2));
   }));
 });
