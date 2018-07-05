bd0de40399906208784dca0d140a823a0da53595
Anders Hessellund Jensen <anders@andershessellund.dk>
2018-07-05 08:54:07 +0200

    datapatch fixes

--- dar10/importDataPatch.js
+++ dar10/importDataPatch.js
@@ -25,29 +25,35 @@ runImporter('importDar10', optionSpec, _.keys(optionSpec), function (args, optio
     yield withImportTransaction(client, 'importDar', (txid) => go(function*() {
       yield client.query(`UPDATE dar1_husnummer set husnummerretning = CASE WHEN ST_X(husnummerretning) >= 0  AND st_X(husnummerretning) < 1
         THEN ST_SetSRID(ST_MakePoint(ST_X(husnummerretning), -ST_Y(husnummerretning)), 25832)
-        ELSE ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832)
+        WHEN st_x(husnummerretning) < 0 THEN ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832) 
+        ELSE husnummerretning
         END`);
       yield client.query(`UPDATE dar1_husnummer_changes set husnummerretning = CASE WHEN ST_X(husnummerretning) >= 0  AND st_X(husnummerretning) < 1 
         THEN ST_SetSRID(ST_MakePoint(ST_X(husnummerretning), -ST_Y(husnummerretning)), 25832)
-        ELSE ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832)
+        WHEN st_x(husnummerretning) < 0 THEN ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832) 
+        ELSE husnummerretning
         END`);
       yield client.query(`UPDATE dar1_husnummer_history set husnummerretning = CASE WHEN ST_X(husnummerretning) >= 0  AND st_X(husnummerretning) < 1 
         THEN ST_SetSRID(ST_MakePoint(ST_X(husnummerretning), -ST_Y(husnummerretning)), 25832)
-        ELSE ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832)
+        WHEN st_x(husnummerretning) < 0 THEN ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832) 
+        ELSE husnummerretning
         END`);
       yield client.query(`UPDATE dar1_husnummer_history_changes set husnummerretning = CASE WHEN ST_X(husnummerretning) >= 0  AND st_X(husnummerretning) < 1 
         THEN ST_SetSRID(ST_MakePoint(ST_X(husnummerretning), -ST_Y(husnummerretning)), 25832)
-        ELSE ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832)
+        WHEN st_x(husnummerretning) < 0 THEN ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832) 
+        ELSE husnummerretning
         END`);
       yield client.query(`UPDATE dar1_husnummer_current set husnummerretning = CASE WHEN ST_X(husnummerretning) >= 0  AND st_X(husnummerretning) < 1 
         THEN ST_SetSRID(ST_MakePoint(ST_X(husnummerretning), -ST_Y(husnummerretning)), 25832)
-        ELSE ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832)
+        WHEN st_x(husnummerretning) < 0 THEN ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832) 
+        ELSE husnummerretning
         END`);
       yield client.query(`UPDATE dar1_husnummer_current_changes set husnummerretning = CASE WHEN ST_X(husnummerretning) >= 0  AND st_X(husnummerretning) < 1 
         THEN ST_SetSRID(ST_MakePoint(ST_X(husnummerretning), -ST_Y(husnummerretning)), 25832)
-        ELSE ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832)
+        WHEN st_x(husnummerretning) < 0 THEN ST_SetSRID(ST_MakePoint(-ST_X(husnummerretning), ST_Y(husnummerretning)), 25832) 
+        ELSE husnummerretning
         END`);
-      yield initialization.reloadDatabaseCode( client, path.join(__dirname, '../psql/schema');
+      yield initialization.reloadDatabaseCode( client, path.join(__dirname, '../psql/schema'));
       yield importDarImpl.importIncremental(client, txid, options.dataDir, false);
     }));
     yield client.query('REFRESH MATERIALIZED VIEW CONCURRENTLY wms_vejpunktlinjer');
