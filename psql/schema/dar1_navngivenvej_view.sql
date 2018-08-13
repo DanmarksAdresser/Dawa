4091c77a02f124ab8f2ebcb0810e8dd489492738
Anders Hessellund Jensen <anders@andershessellund.dk>
2018-08-13 13:29:08 +0200

    Fix navngivne veje med fremtidige ændringsdatoer

--- psql/schema/dar1_navngivenvej_view.sql
+++ psql/schema/dar1_navngivenvej_view.sql
@@ -2,26 +2,28 @@ DROP VIEW IF EXISTS dar1_navngivenvej_view CASCADE;
 CREATE VIEW dar1_navngivenvej_view AS
   SELECT
     n.id,
-    n.status as darstatus,
+    n.status                                                                        AS darstatus,
     (SELECT min(lower(virkning))
      FROM dar1_navngivenvej_history nh
-     WHERE nh.id = n.id) AS oprettet,
+     WHERE nh.id = n.id)                                                            AS oprettet,
     (SELECT MAX(lower(virkning))
      FROM dar1_navngivenvej_history nh
-     WHERE nh.id = n.id) AS ændret,
-    COALESCE(n.vejnavn, '') as navn,
-    COALESCE(n.vejadresseringsnavn, '') as adresseringsnavn,
-    administreresafkommune as administrerendekommune,
+     WHERE nh.id = n.id AND lower(virkning) <= (SELECT virkning
+                                                FROM dar1_meta))                    AS ændret,
+    COALESCE(n.vejnavn, '')                                                         AS navn,
+    COALESCE(n.vejadresseringsnavn,
+             '')                                                                    AS adresseringsnavn,
+    administreresafkommune                                                          AS administrerendekommune,
     beskrivelse,
     retskrivningskontrol,
     udtaltvejnavn,
-    vejnavnebeliggenhed_oprindelse_kilde as beliggenhed_oprindelse_kilde,
-    vejnavnebeliggenhed_oprindelse_nøjagtighedsklasse as beliggenhed_oprindelse_nøjagtighedsklasse,
-    vejnavnebeliggenhed_oprindelse_registrering as beliggenhed_oprindelse_registrering,
-    vejnavnebeliggenhed_oprindelse_tekniskstandard as beliggenhed_oprindelse_tekniskstandard,
-    vejnavnebeliggenhed_vejnavnelinje as beliggenhed_vejnavnelinje,
-    vejnavnebeliggenhed_vejnavneområde as beliggenhed_vejnavneområde,
-    vejnavnebeliggenhed_vejtilslutningspunkter as beliggenhed_vejtilslutningspunkter,
+    vejnavnebeliggenhed_oprindelse_kilde                                            AS beliggenhed_oprindelse_kilde,
+    vejnavnebeliggenhed_oprindelse_nøjagtighedsklasse                               AS beliggenhed_oprindelse_nøjagtighedsklasse,
+    vejnavnebeliggenhed_oprindelse_registrering                                     AS beliggenhed_oprindelse_registrering,
+    vejnavnebeliggenhed_oprindelse_tekniskstandard                                  AS beliggenhed_oprindelse_tekniskstandard,
+    vejnavnebeliggenhed_vejnavnelinje                                               AS beliggenhed_vejnavnelinje,
+    vejnavnebeliggenhed_vejnavneområde                                              AS beliggenhed_vejnavneområde,
+    vejnavnebeliggenhed_vejtilslutningspunkter                                      AS beliggenhed_vejtilslutningspunkter,
     COALESCE(vejnavnebeliggenhed_vejnavnelinje, vejnavnebeliggenhed_vejnavneområde) AS geom
   FROM dar1_navngivenvej_current n
-  WHERE n.status IN (2,3);
+  WHERE n.status IN (2, 3);
