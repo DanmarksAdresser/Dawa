e958effbd5f542eff9296d13afb9fbb14dd36e97
Anders Hessellund Jensen <anders@andershessellund.dk>
2018-10-23 11:46:58 +0200

    Migrering til relation mellem supplerende bynavn og postnumre

--- psql/schema/supplerendebynavn2_postnr_view.sql
+++ psql/schema/supplerendebynavn2_postnr_view.sql
@@ -2,4 +2,5 @@ DROP VIEW IF EXISTS supplerendebynavn2_postnr_view CASCADE;
 
 CREATE VIEW supplerendebynavn2_postnr_view AS
   (select distinct supplerendebynavn_dagi_id, postnr postnr
-  FROM adgangsadresser_mat);
+  FROM adgangsadresser_mat
+  WHERE supplerendebynavn_dagi_id IS NOT NULL);
