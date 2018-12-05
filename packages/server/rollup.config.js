3b96ef8b1a3fb988577da6430b748b09e54854d7
Anders Hessellund Jensen <anders@andershessellund.dk>
2018-12-05 13:09:24 +0100

    Fix issue with building server

--- packages/server/rollup.config.js
+++ packages/server/rollup.config.js
@@ -2,7 +2,6 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import postcss from 'rollup-plugin-postcss'
 import inject from 'rollup-plugin-inject';
 
 export default {
