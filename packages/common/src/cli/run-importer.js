a44474dfa01ab707e73b8695c095622a334d1591
Anders Hessellund Jensen <anders@andershessellund.dk>
2019-02-05 13:30:54 +0100

    Importer timeouts

--- packages/common/src/cli/run-importer.js
+++ packages/common/src/cli/run-importer.js
@@ -4,16 +4,31 @@ const {go} = require('ts-csp');
 const Promise = require('bluebird');
 const cliParameterParsing = require('./cli-parameter-parsing');
 const logger = require('../logger').forCategory('runImporter');
-
-exports.runImporter = (importerName, optionSpec, requiredOptions, importerFn) => {
-  cliParameterParsing.main(optionSpec, requiredOptions, (args, options) => go(function*() {
+const { sleep } = require('../csp-util');
+exports.runImporter = (importerName, optionSpec, requiredOptions, importerFn, importerTimeout) => {
+  if(importerTimeout) {
+    optionSpec.importerTimeout = [false, 'Terminer importer efter den angivne periode (sekunder)', 'integer', importerTimeout]
+  }
+  cliParameterParsing.main(optionSpec, requiredOptions, (args, options) => go(function* () {
+    let timeoutProcess;
+    if (importerTimeout) {
+      timeoutProcess = go(function* () {
+        yield this.delegateAbort(sleep(options.importerTimeout * 1000));
+        logger.error('Importer timed out, aborting', {
+          importerName,
+          timeout: options.importerTimeout
+        });
+        yield sleep(1000);
+        process.exit(1);
+      });
+    }
     try {
       yield importerFn(args, options);
       logger.info("Ran importer", {
         importerName
       });
     }
-    catch(e) {
+    catch (e) {
       logger.error('Importer failed', {
         importerName,
         error: e
@@ -21,6 +36,9 @@ exports.runImporter = (importerName, optionSpec, requiredOptions, importerFn) =>
       // This is a bit hackish, but we add a little delay to ensure that the logs are flushed to disk before exiting.
       yield Promise.delay(1000);
       throw e;
+    }
+    if(timeoutProcess) {
+      timeoutProcess.abort.raise('');
     }
   }).asPromise().done());
 };
