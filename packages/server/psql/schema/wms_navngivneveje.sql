bc7093ece04bf75f5860932ca5b157508ec1fd50
Anders Hessellund Jensen <anders@andershessellund.dk>
2019-03-11 12:24:30 +0100

    WFS/WMS skal kun udstille gældende og foreløbige

--- packages/server/psql/schema/wms_navngivneveje.sql
+++ packages/server/psql/schema/wms_navngivneveje.sql
@@ -1,32 +1,32 @@
 DROP VIEW IF EXISTS wms_vejnavneomraader CASCADE;
 CREATE VIEW wms_vejnavneomraader AS (
-  SELECT
-    id :: TEXT,
-    darstatus,
-    navn,
-    beliggenhed_vejnavneområde as geom
+  SELECT id :: TEXT,
+         darstatus,
+         navn,
+         beliggenhed_vejnavneområde as geom
   FROM navngivenvej_mat
   WHERE beliggenhed_vejnavneområde IS NOT NULL
+    AND darstatus IN (2, 3)
 );
 
 DROP VIEW IF EXISTS wms_vejnavnelinjer CASCADE;
 CREATE VIEW wms_vejnavnelinjer AS (
-  SELECT
-    id :: TEXT,
-    darstatus,
-    navn,
-    beliggenhed_vejnavnelinje as geom
+  SELECT id :: TEXT,
+         darstatus,
+         navn,
+         beliggenhed_vejnavnelinje as geom
   FROM navngivenvej_mat
   WHERE beliggenhed_vejnavnelinje IS NOT NULL
+    AND darstatus IN (2, 3)
 );
 
 DROP VIEW IF EXISTS wms_vejtilslutningspunkter CASCADE;
 CREATE VIEW wms_vejtilslutningspunkter AS (
-  SELECT
-    id :: TEXT,
-    darstatus,
-    navn,
-    beliggenhed_vejtilslutningspunkter as geom
+  SELECT id :: TEXT,
+         darstatus,
+         navn,
+         beliggenhed_vejtilslutningspunkter as geom
   FROM navngivenvej_mat
   WHERE beliggenhed_vejtilslutningspunkter IS NOT NULL
+    AND darstatus IN (2, 3)
 );
