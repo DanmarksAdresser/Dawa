f852f39999c8cf1c10617ef3a4aef1fc2b6b7638
Anders Hessellund Jensen <anders@andershessellund.dk>
2019-03-15 11:16:18 +0100

    Fix test failure

--- packages/replication-client/test/start-server.js
+++ packages/replication-client/test/start-server.js
@@ -1,4 +1,4 @@
-const setupTestServer = require('@dawadk/test-util/src/start-test-server');
+const setupTestServer = require('@dawadk/server/test/helpers/start-test-server');
 
 // the run() function is exposed by mocha, due to the --delay parameter in mocha.opts.
 // we use it to delay executing tests until server is up.
