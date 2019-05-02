35f3380c2c8553d4fbdbd5b1aa32a4f098e1c0d3
Anders Hessellund Jensen <anders@andershessellund.dk>
2019-05-02 15:15:12 +0200

    Fix tidszonefejl i beregning af oprettet tidspunkt for vejstykker

--- packages/server/psql/schema/vejstykker.sql
+++ packages/server/psql/schema/vejstykker.sql
@@ -2,7 +2,12 @@ DROP FUNCTION IF EXISTS vejstykker_init() CASCADE;
 
 DROP VIEW IF EXISTS vejstykker_view;
 CREATE VIEW vejstykker_view AS
-  SELECT id as navngivenvejkommunedel_id,kommunekode,kode,oprettet,vejnavn,adresseringsnavn,navngivenvej_id
-  FROM navngivenvejkommunedel_mat nvk
-  WHERE
-    nvk.darstatus IN (2,3);
+SELECT id                                        as navngivenvejkommunedel_id,
+       kommunekode,
+       kode,
+       oprettet AT TIME ZONE 'Europe/Copenhagen' as oprettet,
+       vejnavn,
+       adresseringsnavn,
+       navngivenvej_id
+FROM navngivenvejkommunedel_mat nvk
+WHERE nvk.darstatus IN (2, 3);
