7b55f4107c2ffab884861e09d7c92b4875265d26
Anders Hessellund Jensen <anders@andershessellund.dk>
2019-10-04 14:19:43 +0200

    Fix til vejmidte-importer

--- packages/server/psql/tableModel.js
+++ packages/server/psql/tableModel.js
@@ -849,6 +849,9 @@ exports.materializations = Object.assign({
       }, {
         table: 'dar1_NavngivenVejKommunedel_current',
         columns: ['id']
+      }, {
+      table: 'vejmidter',
+        columns: ['kommunekode', 'kode']
       }]
   },
   vejnavne_mat: {
