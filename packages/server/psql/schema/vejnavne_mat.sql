a2e72546a16fb1ab5ee14bc2112245d9e04226b9
Anders Hessellund Jensen <anders@andershessellund.dk>
2019-12-05 10:59:22 +0100

    Vejnavne-ressourcen skal ikke udstille det tomme vejnavn

--- packages/server/psql/schema/vejnavne_mat.sql
+++ packages/server/psql/schema/vejnavne_mat.sql
@@ -1,4 +1,5 @@
 DROP VIEW IF EXISTS vejnavne_mat_view;
 CREATE VIEW vejnavne_mat_view AS (
     SELECT DISTINCT vejnavn as navn
-    from navngivenvejkommunedel_mat);
+    from navngivenvejkommunedel_mat
+    WHERE vejnavn <> '');
